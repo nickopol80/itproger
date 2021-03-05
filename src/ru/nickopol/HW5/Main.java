497df9526e93c26057f694ad5300642bdd130a37
18501180 <a18501180@cab-wsm-0007755.sigma.sbrf.ru>
2021-03-05 22:28:50 +0300

    second commit in master branch

--- src/ru/nickopol/HW5/Main.java
+++ src/ru/nickopol/HW5/Main.java
@@ -1,5 +1,15 @@
 package ru.nickopol.HW5;
 
+/**
+ * …or create a new repository on the command line
+ * echo "# java" >> README.md
+ * git init
+ * git add README.md
+ * git commit -m "first commit"
+ * git branch -M master
+ * git remote add origin https://github.com/nickopol80/itproger.git
+ * git push -u origin master
+ */
 public class Main {
     public static void main(String[] args) {
         System.out.println("HW5");
